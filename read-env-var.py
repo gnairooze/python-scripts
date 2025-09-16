d0b3e0d9c8add867ab2840e5b2c2c973bed9406d
George Nairooze <george.nairooze@gmail.com>
2025-09-16 13:57:14 +0300

    create read env var script

---
+++ read-env-var.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+if len(sys.argv) < 2:
+  print("Usage: python script.py <ENV_VAR_NAME>")
+  sys.exit(1)
+
+env_var_name = sys.argv[1]
+value = os.getenv(env_var_name)
+
+print(value)
